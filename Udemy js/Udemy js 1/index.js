de789c2d563dc2da36d91cbf0021afbf44c9b02b
Polina_Melnik <123587146+polinamelnik@users.noreply.github.com>
2023-08-09 13:04:39 +0200

    Add files via upload

---
+++ Udemy js/Udemy js 1/index.js
@@ -0,0 +1,165 @@
+"use strict";
+
+// a = 15;
+// console.log();
+
+// let number = 5;
+// const leftBorderWidht = 1;
+
+// number = 10;
+// console.log(number);
+
+// const odj = {
+//     a: 50
+// };
+
+// obj.a = 10;
+// console.log(odj);
+
+
+// console.log(name);
+// var name = 'polina';
+
+// {
+//     var result = 50;  //!!!нельзя использовать!!!
+// }
+// console.log(result);
+
+// alert(5);
+// [].push('a');
+
+// const vehicleBodyWidht = 5000; 
+// const vehicleBodyleght = 4000;
+// console.log("Ширина кузова автомобиля: " + vehicleBodyWidht + ', длина: ' + vehicleBodyleght);
+
+// ['hsdbvl', 'jkbbf', 'hhkegifwig'].map(a => )
+//Request
+// Data
+// responce
+
+//style
+// snake_case
+//UPER_SNAKE_CASE
+//kebab-case
+//PascalCase
+
+// const COLOR_RED = '#f00';
+
+// let number = (-4.6);
+
+// console.log(4/0);
+// console.log('string' * 9);
+
+// const persone = `5`;
+
+// const bool = false;
+
+// console.log(something);
+
+// let und;
+// console.log(und);
+
+// const obj = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// };
+
+// console.log(obj.name);
+// console.log(obj["name"]);
+
+// let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+// console.log(arr[1]);
+
+
+// const arr = ['a', 'b', 'c'];
+// arr[10] = '3456';
+// console.log(arr);
+
+// const arrobj = {
+//     a: 'a',
+//     '1': 'b',
+//     2: 'c',
+//     abc:{
+//         df: [{}, {}],
+//         raf:{
+
+//         }
+//     }
+
+// };
+
+// const b = 'b';
+
+// // arrobj.b = '1234';
+// arrobj['b'] = [];
+
+// console.log(arrobj.b);
+// console.log(arrobj['b']);
+
+
+
+// // const obj = {a: 1, b: 2 }
+// const obj = {
+//     Anna: 500,
+//     'Alice': 800
+// };
+
+
+// Создаем переменную с названием магазина
+// let name = "My Awesome Store";
+
+// // Создаем объект storeDescription и добавляем свойства
+// let storeDescription = {
+//   budget: 10000,
+//   employees: ["John", "Jane", "Bob"],
+//   products: {
+//     "bier": 19.99,
+//     "Product 2": 29.99
+//   },
+//   open: true
+// };
+
+
+// alert('Hello')
+
+// const result = confirm('are you here?');
+// console.log(result);
+
+// const answer = +prompt("Вам есть 18?", "18")
+// console.log(answer + 5);
+
+// const answers = [];
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваша фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+// console.log(typeof(answers));
+// console.log(typeof(null));
+
+// const category = 'toys';
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = "Polina";
+// alert(`Привет, ${user}`);
+
+// console.log('arr' + " - object");
+// console.log(4 + +"5");  //унарный плюс используется только 1 раз для своих аргументов
+
+// let incr = 10,
+//     decr = 10;
+
+    // incr++;  //оператор инкемента т.е увеличение на 1
+    // decr--;  //оператор декремента т.е уменьшение на 1
+
+// console.log(incr++);
+// console.log(decr--);
+
+// console.log(5%2);
+
+// console.log(2 + 2 * 2 !== '6');
+
+// const isChecked = false,
+//       isClose = false;
+// console.log(isChecked || !isClose);     
+
